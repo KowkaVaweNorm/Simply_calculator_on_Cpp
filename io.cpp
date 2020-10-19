fd30f7f55081687f2e67377c49045e541f09fd7e
Михаил Припачкин <mixakryt2014@mail.ru>
2020-10-19 23:54:26 +0300

    init

---
+++ io.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+//int readNumber()
+//{
+//	int a;
+//	cout << "Input a number: "<< endl;
+//	cin >> a;
+//	return a;
+//}
+//
+//void writeAnswer(int a)
+//{
+//	cout << "a + b = " << a << endl;
+//}
---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+//	int inputNum = readNumber();
+//	int inputNum2 = readNumber();
+//	writeAnswer(inputNum+inputNum2);
+	
+	signed int x{ -10 };
+	int y{ -10 };
+
+	cout << "signed integer: " << x << "And his size" << sizeof(x) << endl;
+	
+	
+	return 0;
+}
